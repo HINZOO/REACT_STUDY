ae1955af4267562317f5d40b8ac31397ffa69d86
HINZOO <hinge1128@gmail.com>
2023-12-10 15:38:47 +0900

    NODE 패키지생성및외부패키지사용

---
+++ Exam_package/index.js
@@ -0,0 +1,9 @@
+const randomColor= require("randomcolor");
+
+let color1 = randomColor();
+let color2 = randomColor();
+let color3 = randomColor();
+
+console.log("랜덤컬러1: "+color1);
+console.log("랜덤컬러2: "+color2);
+console.log("랜덤컬러3: "+color3);
