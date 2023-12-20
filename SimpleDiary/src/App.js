5b212936c4e78d4b6b1e8bf9a61e73c97a7723d2
HINZOO <hinge1128@gmail.com>
2023-12-20 20:07:30 +0900

    리스트 랜더링(조회)

--- SimpleDiary/src/App.js
+++ SimpleDiary/src/App.js
@@ -1,13 +1,45 @@
 import './App.css';
 import DiaryEditor from './DiaryEditor';
+import DiaryList from './DiaryList';
 
+const dummyList = [
+  {
+    id:1,
+    author:"용",
+    content: "난 달에서왔어",
+    emotion:5,
+    created_date: new Date().getTime(),
+  },
+  {
+    id:2,
+    author:"린",
+    content: "어디서왔다고?",
+    emotion:3,
+    created_date: new Date().getTime(),
+  },
+  {
+    id:3,
+    author:"황",
+    content: "뭐라는거야 다 나가",
+    emotion:1,
+    created_date: new Date().getTime(),
+  },
+  {
+    id:4,
+    author:"수연",
+    content: "작가님! 어디가세요!",
+    emotion:2,
+    created_date: new Date().getTime(),
+  },
+]
 function App() {
   return (
     <div className="App">
-
       <DiaryEditor/>
+      <DiaryList diaryList={dummyList} />
     </div>
   );
 }
 
 export default App;
+// created_date: new Date().getTime,//현재시간 기준으로 생성하여 숫자로 변환(getTime)
---
+++ SimpleDiary/src/DiaryList.js
@@ -0,0 +1,26 @@
+import DiaryItem from "./DiaryItem";
+
+const DiaryList = ({diaryList}) =>{
+
+  return <div className="DiaryList">
+    <h2>일기 리스트</h2>
+    <h4>{diaryList.length} 개의 일기가 들어왔어요.</h4>
+    <div>
+      {diaryList.map((it) => (
+        <DiaryItem key={it.id} {...it}/>
+      ))}
+    </div>
+  </div>
+};
+
+//데이터가 안넘어올 경우를 대비하여 빈값 기본값 지정.
+DiaryList.defaultProps={
+  diaryList:[],
+}
+
+export default DiaryList;
+//🍊KEY값
+//<div key={it.id}> 반복하는 값에는 key값이 있어야하며 최상위 태그에 key값을 지정한다. 
+//  {diaryList.map((it,idx) => (
+//       <div key={idx}>
+//key값이 없으면 idx를 사용해도 되지만 나중에 수정,추가,삭제하며 인덱스가 바뀌면 문제가 생길수 있기 때문에 가급적 지양.
